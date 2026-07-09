393bb622b126ce8fb8ea12c98a42e63edc5a2c86
vivian <vivian.g.souza6@aluno.senai.br>
2026-07-09 16:40:30 -0300

    commitado

--- js/produtos.js
+++ js/produtos.js
@@ -1,11 +1,41 @@
 //IMPORTANDO OS PRODUTOS DO ARQUIVO lista_produtos.js
-import {produtos} from './lista_produtos.js'
+import { produtosBellaChic } from './lista_produtos.js'
 
 //PEGANDO ELEMENTOS DO DOM
 const sectionCards = document.querySelector('#cards')
 
 //CARREGANDO OS CARDS
 const listarProdutos = () => {
-    
+
+    sectionCards.innerHTML = ""
+
+    produtosBellaChic.forEach((elem, i) => {
+        const divCard = document.createElement('div')
+        divCard.setAttribute('class', 'card')
+
+        const imgCard = document.createElement('img')
+        imgCard.setAttribute('src', elem.caminho_imagem)
+        imgCard.setAttribute('alt', elem.descricao_produto)
+
+        const pCard = document.createElement('p')
+        pCard.innerHTML = elem.descricao_produto
+
+        const h2Card = document.createElement('h2')
+        h2Card.innerHTML`R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
+
+        const btnCard = document.createElement('button')
+        btnCard.setAttribute('class', 'btn-add')
+        btnCard.innerHTML = 'Adicionar'
+
+        divCard.appendChild(imgCard)
+        divCard.appendChild(pCard)
+        divCard.appendChild(h2Card)
+        divCard.appendChild(btnCard)
+
+        sectionCards.appendChild(divCard)
+
+    })
+
 }
 
+listarProdutos()
