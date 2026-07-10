d0bd73b3fe0b87a2e4dc4770eecaef2ebe50cd51
vivian <vivian.g.souza6@aluno.senai.br>
2026-07-10 16:07:58 -0300

    comitado

--- js/lista_produtos.js
+++ js/lista_produtos.js
@@ -1,405 +1,364 @@
-const produtosBellaChic = [
-
+const produtos = [
     {
-        id_produto: 1,
-        descricao_produto: "Bolsa feminina pequena rosa",
-        valor_unitario: 95.00,
+        idProduto: 1,
+        descricaoProduto: "Brinco Argola Dourada",
+        valorUnitario: 39.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa01.webp",
-        id_secao: 1,
+        caminhoImagem: "../imagens/brincos02.png",
+        idSecao: 1,
+        secao: "Brincos"
+    },
+    {
+        idProduto: 2,
+        descricaoProduto: "Brinco Pérola Clássica",
+        valorUnitario: 34.90,
+        unidade: "UN",
+        caminhoImagem: "../imagens/br00.webp",
+        idSecao: 1,
+        secao: "Brincos"
+    },
+    {
+        idProduto: 3,
+        descricaoProduto: "Brinco Ponto de Luz",
+        valorUnitario: 29.90,
+        unidade: "UN",
+        caminhoImagem: "../imagens/bolsa01.webp",
+        idSecao: 1,
+        secao: "Brincos"
+    },
+    {
+        idProduto: 4,
+        descricaoProduto: "Brinco Flor Delicada",
+        valorUnitario: 42.90,
+        unidade: "UN",
+        caminhoImagem: "../imagens/bolsa ",
+        idSecao: 1,
+        secao: "Brincos"
+    },
+    {
+        idProduto: 5,
+        descricaoProduto: "Brinco Coração Prata",
+        valorUnitario: 36.90,
+        unidade: "UN",
+        caminhoImagem: "img/brincos/brinco-coracao.jpg",
+        idSecao: 1,
+        secao: "Brincos"
+    },
+    {
+        idProduto: 6,
+        descricaoProduto: "Colar Ponto de Luz",
+        valorUnitario: 59.90,
+        unidade: "UN",
+        caminhoImagem: "img/colares/colar-ponto-luz.jpg",
+        idSecao: 2,
+        secao: "Colares"
+    },
+    {
+        idProduto: 7,
+        descricaoProduto: "Colar Coração Dourado",
+        valorUnitario: 69.90,
+        unidade: "UN",
+        caminhoImagem: "img/colares/colar-coracao.jpg",
+        idSecao: 2,
+        secao: "Colares"
+    },
+    {
+        idProduto: 8,
+        descricaoProduto: "Colar Borboleta",
+        valorUnitario: 64.90,
+        unidade: "UN",
+        caminhoImagem: "img/colares/colar-borboleta.jpg",
+        idSecao: 2,
+        secao: "Colares"
+    },
+    {
+        idProduto: 9,
+        descricaoProduto: "Colar Lua e Estrela",
+        valorUnitario: 72.90,
+        unidade: "UN",
+        caminhoImagem: "img/colares/colar-lua-estrela.jpg",
+        idSecao: 2,
+        secao: "Colares"
+    },
+    {
+        idProduto: 10,
+        descricaoProduto: "Colar Trevo",
+        valorUnitario: 61.90,
+        unidade: "UN",
+        caminhoImagem: "img/colares/colar-trevo.jpg",
+        idSecao: 2,
+        secao: "Colares"
+    },
+    {
+        idProduto: 11,
+        descricaoProduto: "Pulseira Riviera",
+        valorUnitario: 79.90,
+        unidade: "UN",
+        caminhoImagem: "img/pulseiras/pulseira-riviera.jpg",
+        idSecao: 3,
+        secao: "Pulseiras"
+    },
+    {
+        idProduto: 12,
+        descricaoProduto: "Pulseira Pérolas",
+        valorUnitario: 54.90,
+        unidade: "UN",
+        caminhoImagem: "img/pulseiras/pulseira-perolas.jpg",
+        idSecao: 3,
+        secao: "Pulseiras"
+    },
+    {
+        idProduto: 13,
+        descricaoProduto: "Pulseira Charms",
+        valorUnitario: 89.90,
+        unidade: "UN",
+        caminhoImagem: "img/pulseiras/pulseira-charms.jpg",
+        idSecao: 3,
+        secao: "Pulseiras"
+    },
+    {
+        idProduto: 14,
+        descricaoProduto: "Pulseira Love",
+        valorUnitario: 49.90,
+        unidade: "UN",
+        caminhoImagem: "img/pulseiras/pulseira-love.jpg",
+        idSecao: 3,
+        secao: "Pulseiras"
+    },
+    {
+        idProduto: 15,
+        descricaoProduto: "Pulseira Cristais",
+        valorUnitario: 68.90,
+        unidade: "UN",
+        caminhoImagem: "img/pulseiras/pulseira-cristais.jpg",
+        idSecao: 3,
+        secao: "Pulseiras"
+    },
+    {
+        idProduto: 16,
+        descricaoProduto: "Anel Solitário",
+        valorUnitario: 45.90,
+        unidade: "UN",
+        caminhoImagem: "img/aneis/anel-solitario.jpg",
+        idSecao: 4,
+        secao: "Anéis"
+    },
+    {
+        idProduto: 17,
+        descricaoProduto: "Anel Ajustável",
+        valorUnitario: 32.90,
+        unidade: "UN",
+        caminhoImagem: "img/aneis/anel-ajustavel.jpg",
+        idSecao: 4,
+        secao: "Anéis"
+    },
+    {
+        idProduto: 18,
+        descricaoProduto: "Anel Borboleta",
+        valorUnitario: 39.90,
+        unidade: "UN",
+        caminhoImagem: "img/aneis/anel-borboleta.jpg",
+        idSecao: 4,
+        secao: "Anéis"
+    },
+    {
+        idProduto: 19,
+        descricaoProduto: "Anel Coração",
+        valorUnitario: 36.90,
+        unidade: "UN",
+        caminhoImagem: "img/aneis/anel-coracao.jpg",
+        idSecao: 4,
+        secao: "Anéis"
+    },
+    {
+        idProduto: 20,
+        descricaoProduto: "Anel Infinito",
+        valorUnitario: 41.90,
+        unidade: "UN",
+        caminhoImagem: "img/aneis/anel-infinito.jpg",
+        idSecao: 4,
+        secao: "Anéis"
+    },
+    {
+        idProduto: 21,
+        descricaoProduto: "Presilha Flor",
+        valorUnitario: 19.90,
+        unidade: "UN",
+        caminhoImagem: "img/cabelo/presilha-flor.jpg",
+        idSecao: 5,
+        secao: "Acessórios para Cabelo"
+    },
+    {
+        idProduto: 22,
+        descricaoProduto: "Tiara Pérolas",
+        valorUnitario: 34.90,
+        unidade: "UN",
+        caminhoImagem: "img/cabelo/tiara-perolas.jpg",
+        idSecao: 5,
+        secao: "Acessórios para Cabelo"
+    },
+    {
+        idProduto: 23,
+        descricaoProduto: "Scrunchie Cetim Rosa",
+        valorUnitario: 14.90,
+        unidade: "UN",
+        caminhoImagem: "img/cabelo/scrunchie-rosa.jpg",
+        idSecao: 5,
+        secao: "Acessórios para Cabelo"
+    },
+    {
+        idProduto: 24,
+        descricaoProduto: "Laço Boutique",
+        valorUnitario: 17.90,
+        unidade: "UN",
+        caminhoImagem: "img/cabelo/laco-boutique.jpg",
+        idSecao: 5,
+        secao: "Acessórios para Cabelo"
+    },
+    {
+        idProduto: 25,
+        descricaoProduto: "Piranha Acrílica",
+        valorUnitario: 22.90,
+        unidade: "UN",
+        caminhoImagem: "img/cabelo/piranha-acrilica.jpg",
+        idSecao: 5,
+        secao: "Acessórios para Cabelo"
+    },
+    {
+        idProduto: 26,
+        descricaoProduto: "Bolsa Transversal Nude",
+        valorUnitario: 119.90,
+        unidade: "UN",
+        caminhoImagem: "img/bolsas/bolsa-nude.jpg",
+        idSecao: 6,
         secao: "Bolsas"
     },
-
     {
-        id_produto: 2,
-        descricao_produto: "Bolsa feminina preta elegante",
-        valor_unitario: 120.00,
+        idProduto: 27,
+        descricaoProduto: "Bolsa Mini Preta",
+        valorUnitario: 99.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa02.webp",
-        id_secao: 1,
+        caminhoImagem: "img/bolsas/bolsa-preta.jpg",
+        idSecao: 6,
         secao: "Bolsas"
     },
-
     {
-        id_produto: 3,
-        descricao_produto: "Bolsa transversal bege",
-        valor_unitario: 80.00,
+        idProduto: 28,
+        descricaoProduto: "Bolsa Tiracolo Rosa",
+        valorUnitario: 109.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa03.webp",
-        id_secao: 1,
+        caminhoImagem: "img/bolsas/bolsa-rosa.jpg",
+        idSecao: 6,
         secao: "Bolsas"
     },
-
     {
-        id_produto: 4,
-        descricao_produto: "Bolsa couro marrom",
-        valor_unitario: 150.00,
+        idProduto: 29,
+        descricaoProduto: "Carteira Feminina",
+        valorUnitario: 69.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa04.jpg",
-        id_secao: 1,
+        caminhoImagem: "img/bolsas/carteira.jpg",
+        idSecao: 6,
         secao: "Bolsas"
     },
-
     {
-        id_produto: 5,
-        descricao_produto: "Bolsa mini vermelha",
-        valor_unitario: 70.00,
+        idProduto: 30,
+        descricaoProduto: "Bolsa Matelassê",
+        valorUnitario: 139.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa05.webp",
-        id_secao: 1,
+        caminhoImagem: "img/bolsas/bolsa-matelasse.jpg",
+        idSecao: 6,
         secao: "Bolsas"
     },
-
     {
-        id_produto: 6,
-        descricao_produto: "Bolsa luxo branca",
-        valor_unitario: 180.00,
+        idProduto: 31,
+        descricaoProduto: "Óculos Gatinho",
+        valorUnitario: 79.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa06.webp",
-        id_secao: 1,
-        secao: "Bolsas"
+        caminhoImagem: "img/oculos/oculos-gatinho.jpg",
+        idSecao: 7,
+        secao: "Óculos"
     },
-
     {
-        id_produto: 7,
-        descricao_produto: "Bolsa executiva feminina",
-        valor_unitario: 160.00,
+        idProduto: 32,
+        descricaoProduto: "Óculos Redondo",
+        valorUnitario: 74.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa07.webp",
-        id_secao: 1,
-        secao: "Bolsas"
+        caminhoImagem: "img/oculos/oculos-redondo.jpg",
+        idSecao: 7,
+        secao: "Óculos"
     },
-
     {
-        id_produto: 8,
-        descricao_produto: "Bolsa ombro preta",
-        valor_unitario: 85.00,
+        idProduto: 33,
+        descricaoProduto: "Óculos Quadrado",
+        valorUnitario: 82.90,
         unidade: "UN",
-        carrinho_imagens: "bolsa08.webp",
-        id_secao: 1,
-        secao: "Bolsas"
+        caminhoImagem: "img/oculos/oculos-quadrado.jpg",
+        idSecao: 7,
+        secao: "Óculos"
     },
-
     {
-        id_produto: 9,
-        descricao_produto: "Carteira feminina rosa",
-        valor_unitario: 45.00,
+        idProduto: 34,
+        descricaoProduto: "Óculos Retrô",
+        valorUnitario: 89.90,
         unidade: "UN",
-        carrinho_imagens: "carteira01.webp",
-        id_secao: 2,
-        secao: "Carteiras"
+        caminhoImagem: "img/oculos/oculos-retro.jpg",
+        idSecao: 7,
+        secao: "Óculos"
     },
-
     {
-        id_produto: 10,
-        descricao_produto: "Carteira feminina preta",
-        valor_unitario: 55.00,
+        idProduto: 35,
+        descricaoProduto: "Óculos Degradê",
+        valorUnitario: 84.90,
         unidade: "UN",
-        carrinho_imagens: "carteira02.webp",
-        id_secao: 2,
-        secao: "Carteiras"
+        caminhoImagem: "img/oculos/oculos-degrade.jpg",
+        idSecao: 7,
+        secao: "Óculos"
     },
-
     {
-        id_produto: 11,
-        descricao_produto: "Carteira compacta bege",
-        valor_unitario: 50.00,
+        idProduto: 36,
+        descricaoProduto: "Relógio Feminino Rose",
+        valorUnitario: 149.90,
         unidade: "UN",
-        carrinho_imagens: "carteira03.webp",
-        id_secao: 2,
-        secao: "Carteiras"
+        caminhoImagem: "img/relogios/relogio-rose.jpg",
+        idSecao: 8,
+        secao: "Relógios"
     },
-
     {
-        id_produto: 12,
-        descricao_produto: "Carteira luxo dourada",
-        valor_unitario: 75.00,
+        idProduto: 37,
+        descricaoProduto: "Relógio Prata",
+        valorUnitario: 139.90,
         unidade: "UN",
-        carrinho_imagens: "carteira04.webp",
-        id_secao: 2,
-        secao: "Carteiras"
+        caminhoImagem: "img/relogios/relogio-prata.jpg",
+        idSecao: 8,
+        secao: "Relógios"
     },
-
     {
-        id_produto: 13,
-        descricao_produto: "Mochila feminina casual",
-        valor_unitario: 130.00,
+        idProduto: 38,
+        descricaoProduto: "Relógio Dourado",
+        valorUnitario: 159.90,
         unidade: "UN",
-        carrinho_imagens: "mochila01.webp",
-        id_secao: 3,
-        secao: "Mochilas"
+        caminhoImagem: "img/relogios/relogio-dourado.jpg",
+        idSecao: 8,
+        secao: "Relógios"
     },
-
     {
-        id_produto: 14,
-        descricao_produto: "Mochila escolar feminina",
-        valor_unitario: 110.00,
+        idProduto: 39,
+        descricaoProduto: "Relógio Minimalista",
+        valorUnitario: 129.90,
         unidade: "UN",
-        carrinho_imagens: "mochila02.webp",
-        id_secao: 3,
-        secao: "Mochilas"
+        caminhoImagem: "img/relogios/relogio-minimalista.jpg",
+        idSecao: 8,
+        secao: "Relógios"
     },
-
     {
-        id_produto: 15,
-        descricao_produto: "Mochila rosa fashion",
-        valor_unitario: 140.00,
+        idProduto: 40,
+        descricaoProduto: "Relógio Pulseira de Couro",
+        valorUnitario: 169.90,
         unidade: "UN",
-        carrinho_imagens: "mochila03.webp",
-        id_secao: 3,
-        secao: "Mochilas"
-    },
-
-    {
-        id_produto: 16,
-        descricao_produto: "Mochila preta elegante",
-        valor_unitario: 155.00,
-        unidade: "UN",
-        carrinho_imagens: "mochila04.webp",
-        id_secao: 3,
-        secao: "Mochilas"
-    },
-
-    {
-        id_produto: 17,
-        descricao_produto: "Bolsa festa dourada",
-        valor_unitario: 90.00,
-        unidade: "UN",
-        carrinho_imagens: "festa01.webp",
-        id_secao: 4,
-        secao: "Festas"
-    },
-
-    {
-        id_produto: 18,
-        descricao_produto: "Clutch preta luxo",
-        valor_unitario: 100.00,
-        unidade: "UN",
-        carrinho_imagens: "festa02.webp",
-        id_secao: 4,
-        secao: "Festas"
-    },
-
-    {
-        id_produto: 19,
-        descricao_produto: "Bolsa festa prata",
-        valor_unitario: 95.00,
-        unidade: "UN",
-        carrinho_imagens: "festa03.webp",
-        id_secao: 4,
-        secao: "Festas"
-    },
-
-    {
-        id_produto: 20,
-        descricao_produto: "Bolsa festa rosa",
-        valor_unitario: 85.00,
-        unidade: "UN",
-        carrinho_imagens: "festa04.webp",
-        id_secao: 4,
-        secao: "Festas"
-    },
-
-    {
-        id_produto: 21,
-        descricao_produto: "Necessaire rosa",
-        valor_unitario: 35.00,
-        unidade: "UN",
-        carrinho_imagens: "necessaire01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 22,
-        descricao_produto: "Necessaire floral",
-        valor_unitario: 40.00,
-        unidade: "UN",
-        carrinho_imagens: "necessaire02.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 23,
-        descricao_produto: "Cinto feminino preto",
-        valor_unitario: 30.00,
-        unidade: "UN",
-        carrinho_imagens: "cinto01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 24,
-        descricao_produto: "Cinto feminino marrom",
-        valor_unitario: 35.00,
-        unidade: "UN",
-        carrinho_imagens: "cinto02.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 25,
-        descricao_produto: "Lenço feminino estampado",
-        valor_unitario: 25.00,
-        unidade: "UN",
-        carrinho_imagens: "lenco01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 26,
-        descricao_produto: "Óculos feminino fashion",
-        valor_unitario: 60.00,
-        unidade: "UN",
-        carrinho_imagens: "oculos01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 27,
-        descricao_produto: "Bolsa casual azul",
-        valor_unitario: 75.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa09.webp",
-        id_secao: 1,
-        secao: "Bolsas"
-    },
-
-    {
-        id_produto: 28,
-        descricao_produto: "Bolsa tiracolo branca",
-        valor_unitario: 90.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa10.webp",
-        id_secao: 1,
-        secao: "Bolsas"
-    },
-
-    {
-        id_produto: 29,
-        descricao_produto: "Carteira vermelha moderna",
-        valor_unitario: 65.00,
-        unidade: "UN",
-        carrinho_imagens: "carteira05.webp",
-        id_secao: 2,
-        secao: "Carteiras"
-    },
-
-    {
-        id_produto: 30,
-        descricao_produto: "Carteira azul feminina",
-        valor_unitario: 58.00,
-        unidade: "UN",
-        carrinho_imagens: "carteira06.webp",
-        id_secao: 2,
-        secao: "Carteiras"
-    },
-
-    {
-        id_produto: 31,
-        descricao_produto: "Mochila viagem feminina",
-        valor_unitario: 170.00,
-        unidade: "UN",
-        carrinho_imagens: "mochila05.webp",
-        id_secao: 3,
-        secao: "Mochilas"
-    },
-
-    {
-        id_produto: 32,
-        descricao_produto: "Mochila pequena rosa",
-        valor_unitario: 100.00,
-        unidade: "UN",
-        carrinho_imagens: "mochila06.webp",
-        id_secao: 3,
-        secao: "Mochilas"
-    },
-
-    {
-        id_produto: 33,
-        descricao_produto: "Bolsa luxo preta",
-        valor_unitario: 220.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa11.webp",
-        id_secao: 1,
-        secao: "Bolsas"
-    },
-
-    {
-        id_produto: 34,
-        descricao_produto: "Bolsa luxo nude",
-        valor_unitario: 210.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa12.webp",
-        id_secao: 1,
-        secao: "Bolsas"
-    },
-
-    {
-        id_produto: 35,
-        descricao_produto: "Pulseira feminina dourada",
-        valor_unitario: 50.00,
-        unidade: "UN",
-        carrinho_imagens: "pulseira01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 36,
-        descricao_produto: "Colar feminino delicado",
-        valor_unitario: 65.00,
-        unidade: "UN",
-        carrinho_imagens: "colar01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 37,
-        descricao_produto: "Brinco feminino dourado",
-        valor_unitario: 35.00,
-        unidade: "UN",
-        carrinho_imagens: "brinco01.webp",
-        id_secao: 5,
-        secao: "Acessórios"
-    },
-
-    {
-        id_produto: 38,
-        descricao_produto: "Bolsa promoção rosa",
-        valor_unitario: 60.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa13.webp",
-        id_secao: 6,
-        secao: "Promoções"
-    },
-
-    {
-        id_produto: 39,
-        descricao_produto: "Bolsa promoção preta",
-        valor_unitario: 55.00,
-        unidade: "UN",
-        carrinho_imagens: "bolsa14.webp",
-        id_secao: 6,
-        secao: "Promoções"
-    },
-
-    {
-        id_produto: 40,
-        descricao_produto: "Kit acessórios feminino",
-        valor_unitario: 120.00,
-        unidade: "KIT",
-        carrinho_imagens: "kit01.webp",
-        id_secao: 6,
-        secao: "Promoções"
+        caminhoImagem: "img/relogios/relogio-couro.jpg",
+        idSecao: 8,
+        secao: "Relógios"
     }
-
 ];
 
-export { produtosBellaChic }
+export {produtos}
--- js/produtos.js
+++ js/produtos.js
@@ -1,41 +1,115 @@
 //IMPORTANDO OS PRODUTOS DO ARQUIVO lista_produtos.js
-import { produtosBellaChic } from './lista_produtos.js'
+import {produtos} from './lista_produtos.js'
 
 //PEGANDO ELEMENTOS DO DOM
-const sectionCards = document.querySelector('#cards')
+const sectioCards = document.querySelector('#cards')
 
 //CARREGANDO OS CARDS
 const listarProdutos = () => {
 
-    sectionCards.innerHTML = ""
 
-    produtosBellaChic.forEach((elem, i) => {
-        const divCard = document.createElement('div')
+    
+
+}
+
+//CHAMANDO A FUNÇÃO listarProdutos
+listarProdutos()
+
+
+//MONTANDO OS MENUS SEÇÕES
+const menuSecoes = () =>{
+    //CRIANDO A COLEÇÃO MAP
+    const mapSecoes = new Map ()
+    //PERCORRENDO O ARRAY PRODUTO
+    produtos.forEach((elem)=>{
+        //SELECIONANDO AS SEÇÔES
+        mapSecoes.set(elem.idSecao, elem)
+    })
+
+//CONVERTENDO MAP EM ARRAY
+    const secoesFiltrada = Array.from(mapSecoes.values())
+
+//RETORNANDO O ARRAY SELECIONADO
+    return secoesFiltrada
+}
+
+//FUNÇÃO PARA INSERIR OS MENUS NA LISTA
+const carregaSecoes = () =>{
+    //PEGANDO O ELEMENTO ul menu-secoes DO DOM
+    const ulMenuSecoes = document.querySelector('#menu-secoes')
+
+    //LIMPANDO O ELEMENTO DO DOM 
+    ulMenuSecoes.innerHTML = ''
+
+    //CHAMANDO A FUNÇÂO menuSecoes E PERCORRENDO O ARRAY DE SEÇÔES JÀ SELECIONADAS
+    menuSecoes().forEach((elem, i)=>{
+        //CRIANDO O ELEMENTO li
+        const liMenu = document.createElement('li')
+
+        //CRIANDO O ELEMENTO a ATRIBUINDO O NOME DA SEÇÃO
+        const aMenu = document.createElement('a')
+        aMenu.setAttribute('href', '#')
+        aMenu.setAttribute('class', 'lnk-secao')
+        aMenu.innerHTML = elem.secao
+
+        aMenu.addEventListener('click',()=>{
+        montaCards(filtroProduto(elem.idSecao))
+        })
+
+        //ADICIONANDO O ELEMENTO FILHO a NO li
+        liMenu.appendChild(aMenu)
+ 
+        //ADICIONANDO O ELEMENTO FILHO liMenu NO OBJETO DOM
+        ulMenuSecoes.appendChild(liMenu)
+    })
+}
+
+carregaSecoes()
+
+//FUNÇÂO FILTRO PRODUTO 
+const filtroProduto = (idSecao)=>{
+    //FILTRANDO OS PRODUTOS A PARTIR DO REPETIÇÃO filter
+    return produtos.filter(elem => elem.idSecao === idSecao)
+}
+
+//FUNÇÃO MONTAR CARDS
+const montaCards = (objProdutos) => {
+     //LIMPANDO A SECTION cards
+    sectioCards.innerHTML = ''
+
+    //PERCORRENDO O ARRAY DE objProdutos
+    objProdutos.forEach((elem, i)=>{
+        //CRIANDO O ELEMNTO div E DEFININDO O ATRIBUTO CARD
+        const divCard= document.createElement('div')
         divCard.setAttribute('class', 'card')
-
-        const imgCard = document.createElement('img')
-        imgCard.setAttribute('src', elem.caminho_imagem)
-        imgCard.setAttribute('alt', elem.descricao_produto)
-
+    
+        //CRIANDO O ELEMENTO img E DEFININDO OS ATRIBUTOS SRC E ALT OS VALORES DO CAMINHO DAS IMAGENS E A DESCRIÇÂO DOS PRODUTOS
+        const imgCard= document.createElement('img')
+        imgCard.setAttribute('src', elem.caminhoImagem)
+        imgCard.setAttribute('alt', elem.descricaoProduto)
+    
+        //CRIANDO O ELEMENTO p E ATRIBUINDO A DESCRIÇÃO DOS PRODUTOS 
         const pCard = document.createElement('p')
-        pCard.innerHTML = elem.descricao_produto
-
+        pCard.innerHTML = elem.descricaoProduto 
+    
+        //CRIANDO O ELEMENTO h2 E ATRIBUINDO O VALOR UNITÁRIO DEIXANDO EM DUAS CASAS DECIMAIS E SUBSTITUINDO PONTO POR VÍRGULA
         const h2Card = document.createElement('h2')
-        h2Card.innerHTML`R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
-
+        h2Card.innerHTML += `R$ ${parseFloat(elem.valorUnitario).toFixed (2).replace('.',',')}`
+    
+        //CRIANDO O ELEMENTO button E DEFININDO OS ATRIBUTOS CLASS E A DESCRIÇÃO ADICIONAR
         const btnCard = document.createElement('button')
         btnCard.setAttribute('class', 'btn-add')
         btnCard.innerHTML = 'Adicionar'
 
+
+        //ADICIONANDO OS ELEMENTOS FILHOS AOS divCard
         divCard.appendChild(imgCard)
         divCard.appendChild(pCard)
         divCard.appendChild(h2Card)
         divCard.appendChild(btnCard)
 
-        sectionCards.appendChild(divCard)
+        //ADICIONANDO O divCard A SECTION CARDS
+        sectioCards.appendChild(divCard)
 
     })
-
-} 
-
-listarProdutos()
+}
